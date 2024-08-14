c7ea6c473aaf427d4c44df154c63ada9f19ece6f
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-08-14 12:22:56 -0300

    chore: adding prettier config for eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import pluginJs from "@eslint/js"
+import eslintConfigPrettier from "eslint-config-prettier"
 import pluginReact from "eslint-plugin-react"
 import globals from "globals"
 import tseslint from "typescript-eslint"
@@ -8,6 +9,7 @@ export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended
+  pluginReact.configs.flat.recommended,
+  eslintConfigPrettier
 ]
 
