d88bdf77f4d95ac62b498c1678b9df155732e3ce
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-08-31 12:05:04 -0300

    chore: updatint eslint rules and adding components packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,9 @@ export default [
   eslintConfigPrettier,
   {
     rules: {
-      "react/react-in-jsx-scope": "off"
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "@typescript-eslint/no-empty-object-type": "off"
     }
   }
 ]
