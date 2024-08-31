5e5e92f1155451b78f4c5c65276cb5083680fb9a
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-08-31 17:53:21 -0300

    feat: implementing resizable component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,6 +2,11 @@ import { Badge } from "@/components/badge"
 import { Input } from "@/components/input"
 import { Label } from "@/components/label"
 import { RadioGroup, RadioGroupItem } from "@/components/radio-group"
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup
+} from "@/components/resizable"
 import { ScrollArea } from "@/components/scroll-area"
 import { Separator } from "@/components/separator"
 import type { Collocation } from "@/lib/scrapper"
@@ -48,67 +53,74 @@ export function App() {
       )}
 
       {!isLoading && (
-        <div className="flex gap-2 h-full w-full items-center justify-center py-3">
-          <ScrollArea className="pr-4 pl-2 py-4 border rounded-md w-1/2 h-full">
-            {collocations.length === 0 && (
-              <p className="text-muted-foreground">No result</p>
-            )}
-            {collocations.length > 0 && (
-              <RadioGroup
-                onValueChange={setSelectedItem}
-                defaultValue={collocations[0].collocationGroup[0].id}>
-                {collocations.map((group, groupIndex) => (
-                  <div key={group.type}>
-                    <p className="text-muted-foreground mb-1">{group.type}</p>
-                    <ul className="space-y-2" key={group.type}>
-                      {group.collocationGroup.map((group, index) => (
-                        <li className="flex relative" key={group.id}>
-                          <RadioGroupItem
-                            autoFocus={index === 0 && groupIndex === 0}
-                            className="peer absolute opacity-0"
-                            value={group.id}
-                            id={group.id}
-                          />
-                          <Label
-                            className="flex justify-between items-center peer-aria-checked:bg-muted w-full px-3 py-2 rounded-md"
-                            htmlFor={group.id}>
-                            {remapType(group.type)}
-                            <span className="rounded-md bg-background px-2 py-1">
-                              {group.collocations.length}
-                            </span>
-                          </Label>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                ))}
-              </RadioGroup>
-            )}
-          </ScrollArea>
-          <ScrollArea className="p-3 border rounded-md w-full h-full">
-            {collocations.length === 0 && (
-              <p className="text-muted-foreground">No result</p>
-            )}
-            {collocations.length > 0 && !!selectedItem && (
-              <div>
-                <div className="flex flex-col gap-1">
-                  <span>Type - {remapType(selectedGroup.type)}</span>
-                  {!!selectedGroup.definition && (
-                    <span>Definition - {selectedGroup?.definition}</span>
-                  )}
-                </div>
-
-                <Separator className="my-3" />
-
-                <div className="flex flex-wrap grow-0 gap-1 overflow-autuuo">
-                  {selectedGroup?.collocations?.map((collocation) => (
-                    <Badge key={collocation}>{collocation}</Badge>
+        <ResizablePanelGroup
+          direction="horizontal"
+          className="flex gap-2 h-full w-full items-center justify-center py-3">
+          <ResizablePanel className="h-full" defaultSize={25}>
+            <ScrollArea className="pr-4 pl-2 py-4 border rounded-md h-full">
+              {collocations.length === 0 && (
+                <p className="text-muted-foreground">No result</p>
+              )}
+              {collocations.length > 0 && (
+                <RadioGroup
+                  onValueChange={setSelectedItem}
+                  defaultValue={collocations[0].collocationGroup[0].id}>
+                  {collocations.map((group, groupIndex) => (
+                    <div key={group.type}>
+                      <p className="text-muted-foreground mb-1">{group.type}</p>
+                      <ul className="space-y-2" key={group.type}>
+                        {group.collocationGroup.map((group, index) => (
+                          <li className="flex relative" key={group.id}>
+                            <RadioGroupItem
+                              autoFocus={index === 0 && groupIndex === 0}
+                              className="peer absolute opacity-0"
+                              value={group.id}
+                              id={group.id}
+                            />
+                            <Label
+                              className="flex justify-between items-center peer-aria-checked:bg-muted w-full px-3 py-2 rounded-md"
+                              htmlFor={group.id}>
+                              {remapType(group.type)}
+                              <span className="rounded-md bg-background px-2 py-1">
+                                {group.collocations.length}
+                              </span>
+                            </Label>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
                   ))}
+                </RadioGroup>
+              )}
+            </ScrollArea>
+          </ResizablePanel>
+          <ResizableHandle className="opacity-0 h-full" />
+          <ResizablePanel className="h-full" defaultSize={50}>
+            <ScrollArea className="p-3 border rounded-md h-full">
+              {collocations.length === 0 && (
+                <p className="text-muted-foreground">No result</p>
+              )}
+              {collocations.length > 0 && !!selectedItem && (
+                <div>
+                  <div className="flex flex-col gap-1">
+                    <span>Type - {remapType(selectedGroup.type)}</span>
+                    {!!selectedGroup.definition && (
+                      <span>Definition - {selectedGroup?.definition}</span>
+                    )}
+                  </div>
+
+                  <Separator className="my-3" />
+
+                  <div className="flex flex-wrap grow-0 gap-1 overflow-autuuo">
+                    {selectedGroup?.collocations?.map((collocation) => (
+                      <Badge key={collocation}>{collocation}</Badge>
+                    ))}
+                  </div>
                 </div>
-              </div>
-            )}
-          </ScrollArea>
-        </div>
+              )}
+            </ScrollArea>
+          </ResizablePanel>
+        </ResizablePanelGroup>
       )}
     </div>
   )
